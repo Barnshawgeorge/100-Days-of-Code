ab468d4c66e713e6e7cd2287e1949e1959697913
Barnshawgeorge <63828154+Barnshawgeorge@users.noreply.github.com>
2022-11-15 09:07:19 -0600

    Delete Day1-BandNameGenerator.py

--- Day1-BandNameGenerator.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to my Band Generator!")
-user_city = input("What city did you grow up in? \n")
-user_pet = input("What is your pets name? \n")
-print("Your Band name is: " + user_city + " " + user_pet)
